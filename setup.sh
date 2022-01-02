e4252bd78f9b9e50e6398a54c933c2ac872d7a69
FarisR99 <farisrehman@hotmail.co.uk>
2022-01-02 13:00:06 +0000

    Fix setup script again

--- setup.sh
+++ setup.sh
@@ -2,27 +2,27 @@
 echo ================================
 echo Setup
 echo ================================
-echo.
+echo ""
 
 echo Installing necessary python libraries
 pip3 install -r requirements.txt
-echo.
+echo ""
 echo ================================
-echo.
+echo ""
 
 echo Installing optional progress bar...
 if ! npm install --global ffmpeg-progressbar-cli; then
   echo "Failed to install optional progress bar."
 fi
-echo.
+echo ""
 echo ================================
-echo.
+echo ""
 
 echo "Downloading shaders..."
 if [[ -d shaders/ ]]; then
   echo "Shaders directory already exists!"
   echo ================================
-  echo.
+  echo ""
   echo "Done!"
   exit
 fi
@@ -35,13 +35,13 @@ if ! wget https://github.com/bloc97/Anime4K/releases/download/v4.0.1/Anime4K_v4.
   echo "Failed to download Anime4K_v4.0.zip from https://github.com/bloc97/Anime4K/releases/download/v4.0.1/Anime4K_v4.0.zip"
   exit
 fi
-if ! mv Anime4K_4.0.zip shaders/; then
+if ! mv Anime4K_v4.0.zip shaders/; then
   echo "Failed to move Anime4K_v4.0.zip to shaders directory"
   exit
 fi
-echo.
+echo ""
 echo ================================
-echo.
+echo ""
 
 echo "Unzipping..."
 if ! unzip shaders/Anime4K_v4.0.zip; then
@@ -50,7 +50,7 @@ if ! unzip shaders/Anime4K_v4.0.zip; then
 fi
 
 rm -rf shaders/Anime4K_v4.0.zip
-echo.
+echo ""
 echo ================================
-echo.
+echo ""
 echo "Done!"
