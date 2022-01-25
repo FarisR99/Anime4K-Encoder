3502634cd653e71cce98f44e4710e5e2de683d91
FarisR99 <farisrehman@hotmail.co.uk>
2022-01-25 23:19:01 +0000

    Clean up output using mode shader

--- shader.py
+++ shader.py
@@ -519,14 +519,16 @@ def start_encoding(codec: str, encoder: str, width: int, height: int,
             remove_audio_and_subs(f, softsubs, softaudio)
             clear()
             print("Encoded files: {0}".format(
-                ", ".join(successful_encodes.keys())))
-            print("Remaining files: {0}".format(", ".join(
+                os.linesep.join(successful_encodes.keys())))
+            print()
+            print("Remaining files: {0}".format(os.linesep.join(
                 [
                     item for item in files
                     if
                     item not in failed_files and item not in successful_encodes
                 ]
             )))
+            print()
             print("Start time: {0}".format(start_time))
             print("Start time for file={0}: {1}".format(str(i + 1),
                                                         current_date()))
